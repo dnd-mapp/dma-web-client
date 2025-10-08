14329b71ae39d6910a37e8f681cb6a86942fb462
NoNamer777 <oscar.wellner@gmail.com>
2025-10-08 12:01:16 +0200

    Rename App to RootComponent

--- src/app/root.component.spec.ts
+++ src/app/root.component.spec.ts
@@ -1,23 +1,23 @@
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { RootComponent } from './root.component';
 
-describe('App', () => {
+describe('RootComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [App],
+            imports: [RootComponent],
             providers: [provideZonelessChangeDetection()],
         }).compileComponents();
     });
 
     it('should create the app', () => {
-        const fixture = TestBed.createComponent(App);
+        const fixture = TestBed.createComponent(RootComponent);
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
     });
 
     it('should render title', () => {
-        const fixture = TestBed.createComponent(App);
+        const fixture = TestBed.createComponent(RootComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
         expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dma-web-client');
--- src/app/root.component.ts
+++ src/app/root.component.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
+    selector: 'dma-root',
     imports: [RouterOutlet],
-    templateUrl: './app.html',
-    styleUrl: './app.scss',
+    templateUrl: './root.component.html',
+    styleUrl: './root.component.scss',
 })
-export class App {
+export class RootComponent {
     protected readonly title = signal('dma-web-client');
 }
