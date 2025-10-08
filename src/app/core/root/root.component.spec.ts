bcaafbe4519bfa171e03a9aae02ad4f2ab01c5f5
NoNamer777 <oscar.wellner@gmail.com>
2025-10-08 12:32:09 +0200

    Refactor test

--- src/app/core/root/root.component.spec.ts
+++ src/app/core/root/root.component.spec.ts
@@ -1,18 +1,30 @@
-import { provideZonelessChangeDetection } from '@angular/core';
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RootComponent } from './root.component';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { RootHarness } from '@dnd-mapp/web-client/testing';
 
 describe('RootComponent', () => {
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [RootComponent],
-            providers: [provideZonelessChangeDetection()],
-        }).compileComponents();
-    });
+    @Component({
+        template: `<dma-root />`,
+        imports: [RootComponent],
+    })
+    class TestComponent {}
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(RootComponent);
-        const app = fixture.componentInstance;
-        expect(app).toBeDefined();
+    async function setupTest() {
+        TestBed.configureTestingModule({
+            imports: [TestComponent],
+        });
+
+        const harnessLoader = TestbedHarnessEnvironment.loader(TestBed.createComponent(TestComponent));
+
+        return {
+            harness: await harnessLoader.getHarness(RootHarness),
+        };
+    }
+
+    it('should create the app', async () => {
+        const { harness } = await setupTest();
+        expect(harness).toBeDefined();
     });
 });
